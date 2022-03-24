64e9d8bbc0dcd79457890adf4b2a1ee95bbd2516
nabilaass <nabilaasshafap@gmail.com>
2022-03-24 15:19:14 +0700

    terserah

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$conn_string = "host=john.db.elephantsql.com " . 
-  "port=5432 dbname=bbabwdoj " .
-  "user=bbabwdoj " . 
-  "password=ymZ9MmRIoXZ_YS8YThBXkn11Zc-e56h5";
+$conn_string = "host=	balarama.db.elephantsql.com " . 
+  "port=5432 dbname=vaxxxonn " .
+  "user=vaxxxonn " . 
+  "password=FIS22sGeO8YMeuqnW77SrXXZpbg5e_S9";
 $conn = pg_connect($conn_string);
 
 if($conn) {
